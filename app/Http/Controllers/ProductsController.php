7b523b6683d24095104635425bbacbe64ee47d98
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2017-10-03 17:08:10 -0300

    Products Controller created

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+
+class ProductsController extends Controller
+{
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function getAll()
+    {
+        return Product::all();
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'price' => 'required|numeric'
+        ]);
+
+        return response()->json(Product::create($request->all()), 201);
+    }
+}

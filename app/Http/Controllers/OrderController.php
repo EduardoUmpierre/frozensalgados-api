f8fb8123bd2515d53d2e30c1ef5ed54efb654f33
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2017-10-31 14:38:33 -0200

    Order controller created

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Order;
+
+class OrderController
+{
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function getAll()
+    {
+        return Order::with(['customer:id,name'])->get();
+    }
+
+    /**
+     * @param $id
+     * @return static
+     */
+    public function getOne($id)
+    {
+        return Order::with(['customer', 'orderProduct', 'orderProduct.product'])->get()->where('id', '=', $id);
+    }
+}
